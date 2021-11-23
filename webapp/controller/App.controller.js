61f32bafea3a17d4027297458226a4900a380f77
molzi <mo@clouddna.at>
2021-11-23 13:40:05 +0000

    Exercise 3.1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,60 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageBox",
-	"sap/ui/core/Fragment"
-], function (Controller, JSONModel, MessageBox, Fragment) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
 	"use strict";
 
 	return Controller.extend("at.clouddna.training00.FioriDeepDive.controller.App", {
 		_fragmentList: {},
 
 		onInit: function () {
-			let oEditModel = new JSONModel({
-				editmode: false
-			});
 
-			this.getView().setModel(oEditModel, "editModel");
-
-			this._showCustomerFragment("DisplayCustomer");
-		},
-
-		onEditPress: function (oEvent) {
-			this._toggleEdit(true);
-		},
-
-		onSavePress: function (oEvent) {
-			let oCustomer = this.getView().getModel().getData();
-
-			this._toggleEdit(false);
-			MessageBox.information(JSON.stringify(oCustomer));
-
-		},
-
-		_toggleEdit: function (bEditMode) {
-			let oEditModel = this.getView().getModel("editModel");
-
-			oEditModel.setProperty("/editmode", bEditMode);
-
-			this._showCustomerFragment(bEditMode ? "EditCustomer" : "DisplayCustomer");
-		},
-
-		_showCustomerFragment: function (sFragmentName) {
-			let oPage = this.getView().byId("page");
-
-			oPage.removeAllContent();
-			if (this._fragmentList[sFragmentName]) {
-				oPage.insertContent(this._fragmentList[sFragmentName]);
-			} else {
-				Fragment.load({
-					id: this.getView().createId(sFragmentName),
-					name: "at.clouddna.training00.FioriDeepDive.view." + sFragmentName,
-					controller: this
-				}).then(function (oFragment) {
-					this._fragmentList[sFragmentName] = oFragment;
-					oPage.insertContent(oFragment);
-				}.bind(this));
-			}
 		}
 	});
 });
