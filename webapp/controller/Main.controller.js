4fd916e530441494db710f04ecb478e9e2c6a92c
molzi <mo@clouddna.at>
2021-11-24 10:19:04 +0000

    Exercise 5.1

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,44 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("at.clouddna.training00.FioriDeepDive.controller.Main", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
 		onInit: function () {
 
 		},
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+		onDeleteCustomerPress: function (oEvent) {
+			let sCustomerPath = oEvent.getSource().getBindingContext().sPath,
+				oModel = this.getView().getModel(),
+				oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			MessageBox.confirm(oBundle.getText("dialog.delete"), {
+				onClose: function (sAction) {
+					if (sAction === MessageBox.Action.OK) {
+						oModel.remove(sCustomerPath, {
+							success: function () {
+								MessageBox.success(oBundle.getText("dialog.delete.success"));
+							},
+							error: function (oError) {
+								MessageBox.error(oError.message);
+							}
+						});
+					}
+				}
+			});
+		}
 
 	});
 
